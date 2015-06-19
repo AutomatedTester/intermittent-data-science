bafe06d2a7e8e49dbe1ccfda6c495aaceabed29a
AutomatedTester <dburns@mozilla.com>
2015-06-19 01:15:02 +0100

    show intermittents closed in last 120 days

--- bug_search.py
+++ bug_search.py
@@ -15,6 +15,27 @@ def get_intermittents_created_in_last(days=7):
                    .timeframe(seven_days_ago, today)\
                    .search()
 
+def get_intermittents_closed(days=7):
+    tday = datetime.date.today()
+    tday_minus_7 = date_as_of(days)
+    today = '%s-%s-%s' %(tday.year, tday.month if tday.month >= 10 else '0%s' % tday.month, tday.day)
+    seven_days_ago = '%s-%s-%s' %(tday_minus_7.year, tday_minus_7.month if tday_minus_7.month >= 10 else
+                 '0%s' % tday_minus_7.month, tday_minus_7.day)
+    bugzilla = bugsy.Bugsy()
+    bugs = bugzilla.search_for\
+                   .keywords("intermittent-failure")\
+                   .change_history_fields(['bug_status'], 'Resolved')\
+                   .timeframe(seven_days_ago, today)\
+                   .search()
+    result= {}
+    for bug in bugs:
+        try:
+            result[bug.resolution] = result[bug.resolution] + 1
+        except:
+            result[bug.resolution] = 1
+
+    for k,v in result.iteritems(): 
+        print "%s: %s" % (k, v)
 
 def date_as_of(days):
     tday = datetime.datetime.today()
@@ -28,7 +49,7 @@ def amount_of_comments_per_bug():
     for bug in bugs:
         if bug.product == "Thunderbird" \
             or bug.status.upper() == "RESOLVED"\
-            or "[leave open]" in bug._bug["whiteboard"]
+            or "[leave open]" in bug._bug["whiteboard"] \
             or "orange-bomb" in bug._bug["whiteboard"]:
             continue
         comments = bug.get_comments()
@@ -78,5 +99,6 @@ def bugs_possibly_ready_to_close():
     print("The following bugs need to be reviewed %s" % set(bugs_to_review))
 
 if __name__ == '__main__':
-    bugs_possibly_ready_to_close()
+    #bugs_possibly_ready_to_close()
     #amount_of_comments_per_bug()
+    get_intermittents_closed(120)
